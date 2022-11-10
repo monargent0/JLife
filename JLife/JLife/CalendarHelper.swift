38bcc64f87bd2f4b718d155b34e8a97a74712e81
monargent0 <owjddms1118@gmail.com>
2022-11-11 06:46:01 +0900

    달력 기능 구현 완료

--- JLife/JLife/CalendarHelper.swift
+++ JLife/JLife/CalendarHelper.swift
@@ -1,3 +1,10 @@
+//
+//  CalendarHelper.swift
+//  JLife
+//
+//  Created by 오정은 on 2022/11/11.
+//
+
 import Foundation
 import UIKit
 
@@ -9,18 +16,19 @@ class CalenderHelper{
     func plusMonth(date: Date) -> Date {
         return calendar.date(byAdding: .month, value: 1, to: date)!
     }
+    
     // 이전달
     func minusMonth(date: Date) -> Date {
         return calendar.date(byAdding: .month, value: -1, to: date)!
     }
-    // Month 표현 (1월)
+    
+    // Month 표현 (1)
     func monthString(date: Date) -> String {
         let dateFormatter = DateFormatter()
-//        dateFormatter.locale = Locale(identifier: "en_US") // 영어로 표현하기 위함
-//        dateFormatter.dateFormat = "MMMM" // Full name ex) December
         dateFormatter.dateFormat = "M" // M : 1 ~ 12 / MM : 01 ~ 12
         return dateFormatter.string(from: date)
     }
+    
     // Year 표현 (2022)
     func yearString(date: Date) -> String {
         let dateFormatter = DateFormatter()
@@ -28,38 +36,49 @@ class CalenderHelper{
         return dateFormatter.string(from: date)
     }
     
-    // 월 일수 ex) InPut 10월 16일 -> Output 10월 총 일수 : 31
+    // 월의 총 일 수 ex) InPut: 10월 16일 -> Output: 10월 총 일수 : 31
     func daysInMonth(date:Date) -> Int {
         let range = calendar.range(of: .day, in: .month, for: date)!
         return range.count
     }
     
-    // Today 일자 ex) Input 10월 16일 -> Output 16
+    // CURRENT DATE Today 일자 ex) Input: 10월 16일 -> Output: 16
     func dayOfMonth(date:Date) -> Int {
         let components = calendar.dateComponents([.day], from: date)
         return components.day!
     }
     
-    // 월의 첫날  print(components) = year: 2022 month: 10 isLeapMonth: false
+    // 월의 첫날
+    // components출력 : year: 2022 month: 11 isLeapMonth: false (윤달)
+    // return 값 : 2022-10-31 15:00:00 +0000
     func firstOfMonth(date:Date) -> Date {
         let components = calendar.dateComponents([.year, .month], from: date)
         return calendar.date(from: components)!
     }
-    
-    // 첫날이 언제인지 sun~sat : 0~6
-    func weekDay(date:Date) -> Int {
+          
+    // 월의 첫날이 몇번째 요일인지 Sun ~ Sat : 0 ~ 6 (-1 적용)
+    func startWeekDay(date:Date) -> Int {
         let components = calendar.dateComponents([.weekday], from: date)
         return components.weekday! - 1
     }
     
-    // 전달이 몇일인지
-    func prevDays(date:Date) -> Int {
+    // 이전달 총 일수
+    func daysInPrevMonth(date:Date) -> Int {
         let prevDate = calendar.date(byAdding: .month, value: -1, to: date)!
         let range = calendar.range(of: .day, in: .month, for: prevDate)!
         return range.count
     }
     
-    func a(date:Date) {
-        prevDays(date: date)
+    //--- View Part ---
+    // 위 테두리
+    func addTopBorder(stackview : UIStackView , borderWidth : Double , borderColor : CGColor){
+        let topLine = CALayer()
+        topLine.frame = CGRect(x: 0 , y: 0 , width: UIScreen.main.bounds.width , height: CGFloat(borderWidth) )
+        // x,y : 시작 위치 & w,h : border 너비,두께
+        // UIScreen.main.bounds.width /  stackview.bounds.size.width
+        topLine.backgroundColor = borderColor // UIColor.darkGray.cgColor
+        stackview.layer.masksToBounds = true // true : 내 영역(Layer) 이외 영역의 Sub Layer는 Draw 하지 않는다
+        stackview.layer.addSublayer(topLine)
     }
+
 }
--- JLife/JLife/MainViewController.swift
+++ JLife/JLife/MainViewController.swift
@@ -9,13 +9,68 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    //
+    @IBOutlet weak var cvCalender: UICollectionView! // 달력 CollectionView
+    @IBOutlet weak var svWeekDays: UIStackView!
+    @IBOutlet weak var lblMonthYear: UILabel!
+    @IBOutlet weak var svBottom: UIStackView!
+    //
+    var presentDate = Date()
+    var allDateItems : [String] = []
+    var items : (daysInMonth : Int, startWeekDay : Int) = (0,0)
+    
+    //
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setMonth() // 달력 구성
+        cvCalender.delegate = self
+        cvCalender.dataSource = self
+        CalenderHelper().addTopBorder(stackview: svWeekDays, borderWidth: 1.5 , borderColor: UIColor.darkGray.cgColor)
+        CalenderHelper().addTopBorder(stackview: svBottom, borderWidth: 1.5, borderColor: UIColor.darkGray.cgColor)
     }
     
-
+    // Previous Month
+    @IBAction func btnPrevMonth(_ sender: UIButton) {
+        presentDate = CalenderHelper().minusMonth(date: presentDate)
+        setMonth()
+    }
+    // Next Month
+    @IBAction func btnNextMonth(_ sender: UIButton) {
+        presentDate = CalenderHelper().plusMonth(date: presentDate)
+        setMonth()
+    }
+    
+    //--- Setting Calendar Function ---
+    func setMonth(){
+        // Properties
+        allDateItems.removeAll() // 초기화
+        items.daysInMonth = CalenderHelper().daysInMonth(date: presentDate) // 현재 달 총 일수
+        let firstOfMonth = CalenderHelper().firstOfMonth(date: presentDate) // 월 첫날
+        items.startWeekDay = CalenderHelper().startWeekDay(date: firstOfMonth) // 달 시작 요일
+        let daysInPrevMonth = CalenderHelper().daysInPrevMonth(date: presentDate) // 이전 달 총 일수
+        
+        // Contents
+        var count:Int = 1
+        while(count <= 42 ){ // 7days 6week
+            if(count <= items.startWeekDay){
+            // 이전 달
+                allDateItems.append(String(daysInPrevMonth - items.startWeekDay + count))
+            }else if(count - items.startWeekDay > items.daysInMonth){
+            // 다음 달
+                allDateItems.append(String( count % (items.daysInMonth + items.startWeekDay) ))
+            }else{
+            // 현재 달
+                allDateItems.append(String(count - items.startWeekDay))
+            }
+            count += 1
+        } // while
+        
+        lblMonthYear.text = CalenderHelper().yearString(date: presentDate) + "년 " + CalenderHelper().monthString(date: presentDate) + "월"
+        
+        // CollectionView Reload
+        cvCalender.reloadData()
+    }// func setMonth
+    
     /*
     // MARK: - Navigation
 
@@ -26,4 +81,48 @@ class MainViewController: UIViewController {
     }
     */
 
-}
+} // MainViewController
+
+//--- Collection View Cell ---
+extension MainViewController:UICollectionViewDelegate, UICollectionViewDataSource{
+    // Cell 갯수
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        allDateItems.count
+    }
+    // Cell 구성
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "dayCell", for: indexPath) as! CalendarCell
+        // Date
+        cell.lblDay.text = allDateItems[indexPath.row]
+        // Font Color
+        switch indexPath.row {
+        case 0..<items.startWeekDay:
+            cell.lblDay.textColor = .lightGray
+        case (items.daysInMonth + items.startWeekDay)..<allDateItems.count:
+            cell.lblDay.textColor = .lightGray
+        default:
+            cell.lblDay.textColor = .darkGray
+        }
+        // Layer Color
+        //
+//        cell.backgroundColor = .cyan
+        return cell
+    }
+} //-
+extension MainViewController:UICollectionViewDelegateFlowLayout{
+    // 위아래 간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1.1
+    }
+    // 좌우 간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1.2
+    }
+    // Cell 크기
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (collectionView.frame.size.width) / 7 - 1
+        let size = CGSize(width: width, height: width)
+        
+        return size
+    }
+} //-
