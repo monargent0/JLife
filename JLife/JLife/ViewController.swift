ae001e139da50a21bef205402787a34761d19c3b
monargent0 <owjddms1118@gmail.com>
2023-06-22 03:03:19 +0900

    App 아이콘 폴더 추가

---
+++ JLife/JLife/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JLife
+//
+//  Created by 오정은 on 2023/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
