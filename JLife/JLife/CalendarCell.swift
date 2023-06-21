b1733bdca19eac76d7b21a90b8c8f29733a2d4ce
monargent0 <owjddms1118@gmail.com>
2023-06-22 04:48:39 +0900

    달력 오토레이아웃 에러

--- JLife/JLife/CalendarCell.swift
+++ JLife/JLife/CalendarCell.swift
@@ -8,5 +8,18 @@
 import UIKit
 
 class CalendarCell: UICollectionViewCell {
+    // storyborad와 연결
+    @IBOutlet weak var lblDay: UILabel!
     
+    // Layer Shape (원)
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        self.layer.cornerRadius = self.frame.size.width / 2
+    }
+    
+    // Prepare For Reuse (초기화)
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.backgroundColor = .clear
+    }
 }
--- JLife/JLife/MainViewController.swift
+++ JLife/JLife/MainViewController.swift
@@ -9,12 +9,53 @@ import UIKit
 
 class MainViewController: UIViewController {
 
+    /* 스토리보드와 연결 */
+    @IBOutlet weak var cvCalendar: UICollectionView! // 달력
+    @IBOutlet weak var lblDateTitle: UILabel! // 상단 년월
+
+    /* 변수 선언 */
+    var presentDate = Date() // 현재
+    var allDateItems : [String] = [] //
+    var items : (daysInMonth : Int , startWeekDay : Int) = (0,0) //
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        cvCalendar.dataSource = self
+        cvCalendar.delegate = self
+        setMonth()
         // Do any additional setup after loading the view.
     }
     
+    /* 버튼 */
+    
+    /* Setting Calendar */
+    func setMonth(){
+        // Properties
+        allDateItems.removeAll() // 초기화
+        items.daysInMonth = CalendarBuilder().daysInMonth(date: presentDate) // 현재 달 일수
+        let firstOfMonth = CalendarBuilder().firstOfMonth(date: presentDate) // 월의 첫날
+        items.startWeekDay = CalendarBuilder().startWeekDay(date: firstOfMonth) // 달 시작 요일
+        let daysInPrevMonth = CalendarBuilder().daysInPrevMonth(date: presentDate) // 이전 달 일수
+        
+        // Calendar Contents
+        var count:Int = 1
+        while(count <= 42){ // 7 days 6 week
+            if(count <= items.startWeekDay){ // 이전 달
+                allDateItems.append(String(daysInPrevMonth - items.startWeekDay + count))
+            }else if(count - items.startWeekDay > items.daysInMonth){ // 다음 달
+                allDateItems.append(String(count % (items.daysInMonth + items.startWeekDay)))
+            }else{ // 현재 달
+                allDateItems.append(String(count - items.startWeekDay))
+            }
+            count += 1
+        } // while
+        
+        // Title
+        lblDateTitle.text = CalendarBuilder().yearString(date: presentDate) + "년 " + CalendarBuilder().monthString(date: presentDate) + "월"
+        
+        // CollectionView Reload
+        cvCalendar.reloadData()
+    }// Func setMonth
 
     /*
     // MARK: - Navigation
@@ -26,4 +67,47 @@ class MainViewController: UIViewController {
     }
     */
 
-}
+} //MainViewController
+
+/* Collection View Cell */
+extension MainViewController:UICollectionViewDelegate, UICollectionViewDataSource{
+    // Cell 개수
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        allDateItems.count
+    }
+    // Cell 구성
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "dayCell", for: indexPath) as! CalendarCell
+        // Date
+        cell.lblDay.text = allDateItems[indexPath.row]
+        // Font Color
+        switch indexPath.row {
+        case 0..<items.startWeekDay:
+            cell.lblDay.textColor = .lightGray
+        case (items.daysInMonth + items.startWeekDay)..<allDateItems.count:
+            cell.lblDay.textColor = .lightGray
+        default:
+            cell.lblDay.textColor = .darkGray
+        }
+        return cell
+    }
+}// Delegate, DataSouce
+
+extension MainViewController:UICollectionViewDelegateFlowLayout{
+    // 위아래 간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 1.0
+    }
+    // 좌우 간격
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1.0
+    }
+    // Cell 크기
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = (collectionView.frame.size.width) / 7 - 1
+        let size = CGSize(width: width, height: width)
+        return size
+    }
+    //
+    
+}// DelegateFlowLayout
