7ba099008d6c86de97cd360067bb3d3e4a1212ec
monargent0 <owjddms1118@gmail.com>
2023-07-02 03:26:49 +0900

    요일 팝업창 구성

---
+++ JLife/JLife/DailyPopUpViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DailyPopUpViewController.swift
+//  JLife
+//
+//  Created by 오정은 on 2023/07/02.
+//
+
+import UIKit
+
+class DailyPopUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // 화면 half 사이즈
+        if let sheet = sheetPresentationController {
+            sheet.detents = [.medium()]
+        }
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
