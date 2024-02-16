840e1f9ee25fbb0df84f4101946f30febd98f9ae
monargent0 <owjddms1118@gmail.com>
2024-02-16 16:40:06 +0900

    색상 테마 Assets 생성

--- JLife/JLife/MainViewController.swift
+++ JLife/JLife/MainViewController.swift
@@ -426,10 +426,12 @@ extension MainViewController:UICollectionViewDelegate, UICollectionViewDataSourc
             cell.isUserInteractionEnabled = false
         default:
             for score in scoreData{
-                if score.day == allDateItems[indexPath.row]{
-                    cell.backgroundColor = UIColor(named: "ScoreColor")?.withAlphaComponent(CGFloat(Double(score.score!) / 100.0))
+                if score.day == allDateItems[indexPath.row]{ // Color Theme 변경 위치
+//                    cell.backgroundColor = UIColor(named: "ScoreColor")?.withAlphaComponent(CGFloat(Double(score.score!) / 100.0))
+                    cell.backgroundColor = UIColor(named: "ZTheme")?.withAlphaComponent(CGFloat(Double(score.score!) / 100.0))
                     if Double(score.score!) / 100.0 == 1.0 {
-                        cell.layer.borderColor = UIColor(named: "PinkColor")?.cgColor
+//                        cell.layer.borderColor = UIColor(named: "PinkColor")?.cgColor
+                        cell.layer.borderColor = UIColor(named: "ZBorder")?.cgColor
                     }
                 }
             }
