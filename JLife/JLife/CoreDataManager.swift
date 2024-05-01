d83363183b9d3d9070ea9668184268fce9a67f55
Serena <sha0720@naver.com>
2024-05-01 18:00:26 +0900

    [JLIFE-4] CoreDataManagerProtocol 생성 및 준수, 이에 Opaque타입을 Boxed Protocol 타입으로 변환

--- JLife/JLife/CoreDataManager.swift
+++ JLife/JLife/CoreDataManager.swift
@@ -8,10 +8,16 @@
 import CoreData
 
 protocol CoreDataManagerProtocol {
+    associatedtype T: NSFetchRequestResult
     
+    func fetch(request: NSFetchRequest<T>) throws -> [T]
+    func insert(entityName: String, entityProperties: [String: Any])
+    func update(object: NSFetchRequest<T>, entityProperties: [String: Any])
+    func deleteData(object: NSFetchRequest<T>)
+    func isExistData(object: NSFetchRequest<T>) -> Bool
 }
 
-class CoreDataManager: CoreDataManagerProtocol {
+class CoreDataManager<T: NSFetchRequestResult>: CoreDataManagerProtocol {
     private let name: String
     
     private lazy var persistentContainer: NSPersistentContainer = {
@@ -32,7 +38,7 @@ class CoreDataManager: CoreDataManagerProtocol {
     }
     
     // MARK: - CRUD
-    func fetchData(request: NSFetchRequest<NSFetchRequestResult>) throws -> [some NSFetchRequestResult] {
+    func fetch(request: NSFetchRequest<T>) throws -> [T] {
         let data = try context.fetch(request)
         
         return data
@@ -50,7 +56,7 @@ class CoreDataManager: CoreDataManagerProtocol {
         }
     }
     
-    func update(object: NSFetchRequest<NSFetchRequestResult>, entityProperties: [String: Any]) {
+    func update(object: NSFetchRequest<T>, entityProperties: [String: Any]) {
         guard let updatedObject = request(object) else { return }
         
         for entityProperty in entityProperties {
@@ -60,21 +66,21 @@ class CoreDataManager: CoreDataManagerProtocol {
         saveContext()
     }
     
-    func deleteData(object: NSFetchRequest<NSFetchRequestResult>) {
+    func deleteData(object: NSFetchRequest<T>) {
         guard let deleteObject = request(object) else { return }
         
         context.delete(deleteObject)
         saveContext()
     }
     
-    func isExistData(object: NSFetchRequest<NSFetchRequestResult>) -> Bool {
+    func isExistData(object: NSFetchRequest<T>) -> Bool {
         request(object) != nil ? true : false
     }
     
     // MARK: - Private Function
-    private func request(_ fetchObject: NSFetchRequest<NSFetchRequestResult>) -> NSManagedObject? {
+    private func request(_ fetchObject: NSFetchRequest<T>) -> NSManagedObject? {
         do {
-            guard let object = try fetchData(request: fetchObject).first as? NSManagedObject else { return nil }
+            guard let object = try fetch(request: fetchObject).first as? NSManagedObject else { return nil }
             
             return object
         } catch {
