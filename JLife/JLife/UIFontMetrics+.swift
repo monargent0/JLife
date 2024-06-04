2bdd06790904311d14a24295a0b68c8e0bede440
Serena <sha0720@naver.com>
2024-06-04 15:34:09 +0900

    [JLIFE-10] UIFontMetrics에서 UserDefault로 받은 Font name 옵셔널 바인딩

--- JLife/JLife/UIFontMetrics+.swift
+++ JLife/JLife/UIFontMetrics+.swift
@@ -8,8 +8,15 @@
 import UIKit
 
 extension UIFontMetrics {
-  static func customFont(with name: String, of size: CGFloat, for style: UIFont.TextStyle) -> UIFont {
-    let font = UIFont(name: name, size: size)!
+  static func customFont(with name: String?, of size: CGFloat, for style: UIFont.TextStyle) -> UIFont {
+    let fontName = name ?? FontStyle.surroundAirStyle.name
+    guard let font = UIFont(name: fontName, size: size) else {
+      fatalError("""
+          Failed to load the "\(fontName)" font.
+          Make sure the font file is included in the project and the font name is spelled correctly.
+          """
+      )
+    }
     
     return UIFontMetrics(forTextStyle: style).scaledFont(for: font)
   }
